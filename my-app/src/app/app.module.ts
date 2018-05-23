4d679585142844b1af98c30e38067921c9b92a9b
Sivajiganesh Kandimalla <Sivajiganesh.Kandimalla2@T-mobile.com>
2018-05-22 20:59:58 -0700

    changing the codes

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -22,7 +22,7 @@ import { AuthEffects } from './auth/store/auth.effects';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({appId:'my-recipe-app'}),
     HttpClientModule,
     AppRoutingModule,
     SharedModule,
